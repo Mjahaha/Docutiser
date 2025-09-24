d95234dde1e110d06d150a4e3e6667625105f4f4
Jay Bailey <jaybaileycs@gmail.com>
2025-09-25 09:38:13 +1000

    Frameworks from JSON

---
+++ src/utils/frameworks.tsx
@@ -0,0 +1,29 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+interface FrameworkRequirement {
+  requirement_id: number;
+  requirement_name: string;
+}
+
+interface Framework {
+  id: number;
+  name: string;
+  description: string;
+  requirements: FrameworkRequirement[];
+}
+
+export async function getFramework(id: number): Promise<Framework> {
+  // Read the JSON file
+  const filePath = path.join(process.cwd(), 'src', 'app', 'test_data', 'framework_test_data.json');
+  const fileContents = await fs.readFile(filePath, 'utf8');
+  const frameworks: Framework[] = JSON.parse(fileContents);
+  
+  // Find the framework with the matching id
+  const framework = frameworks.find(a => a.id === id);
+  
+  if (!framework) {
+    throw new Error(`Framework with id ${id} not found`);
+  }
+  return framework;
+}
