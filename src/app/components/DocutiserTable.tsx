b857a20aa1b96b829a231df8832f1e5259cd33d4
James Pridgeon <sleeptightwakebright@hotmail.com>
2025-10-04 17:53:51 +1000

    added DocutiserTable component and added it into frameworks

---
+++ src/app/components/DocutiserTable.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { ReactNode } from "react";
+import Link from "next/link";
+
+interface Column {
+  label: string,
+  key: string
+}
+
+interface TableProps {
+  data: Record<string, any>[],
+  columns: Column[]
+}
+
+/** A reusable table component to table in data[] and show 
+ * 
+ * @param data, an array of objects to display as rows
+ * @param columns, an array describing the which keys in our data get columns 
+ * Column { label: string, key: string } 
+ * @returns a Table component populated with the data given
+ */
+export default function DocutiserTable ( { data, columns } : TableProps ) {
+  
+  // Function to create header
+  const createHeader = () => (
+    <thead className="bg-gray-100 border-b">
+      <tr className="text-left">
+        { 
+          columns.map( (col : Column) => (
+            <th key={col.key} className="px-4 py-2">
+              {col.label}
+            </th>
+          ))
+        }
+      </tr>
+    </thead>
+  )
+
+  // Function to create row
+  const createRow = (rowData : Record<string, ReactNode>) => (
+    <tr className="">
+      {
+        columns.map( (col : Column) => (
+          <td className="px-4 py-2 border-gray-300">
+            {
+              rowData[col.key]
+            }
+          </td>
+        ))
+      }
+    </tr>
+  )
+
+  // Function to create body, utilisation createRow function
+  const createBody = () => (
+    <tbody className="">
+      {
+        data.map( (thisData) => (
+          createRow(thisData)
+        ))
+      }
+    </tbody>
+  )
+
+  return (
+    <table className="w-full border p-4 space-y-2">
+      {createHeader()}
+      {createBody()}
+    </table>
+  )
+}
+
--- src/app/frameworks/page.tsx
+++ src/app/frameworks/page.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
 import { getFrameworks } from "@/utils/frameworks";
-import { FrameworkList } from "./frameworkList";
+import { FrameworkList } from "./FrameworkList";
+import DocutiserTable from "../components/DocutiserTable";
+
+interface Column {
+  label: string,
+  key: string
+}
 
 export default async function Frameworks() {
   const frameworkData = await getFrameworks();
+  const frameworkColumnsForTable : Column[] = [
+    { label: 'Framework Name', key: 'name'},
+    { label: 'Framework Description', key: 'description'},
+  ]
+  console.log(frameworkData)
   return (
     <div className="p-8 space-y-8">
       {/* Back button */}
@@ -16,6 +27,8 @@ export default async function Frameworks() {
       <h1 className="text-center text-xl font-bold">Frameworks</h1>
 
       <FrameworkList frameworks={frameworkData} />
+      <DocutiserTable data={frameworkData} columns={frameworkColumnsForTable} />
+      
 
       <Link href="/frameworks/builder" className="border px-4 py-2">
         + New Framework
